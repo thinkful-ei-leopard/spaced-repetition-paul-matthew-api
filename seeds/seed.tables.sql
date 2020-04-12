9236892488380b599acfb6428037fd924341bc67
wavinginspace <paulbaisley@gmail.com>
2020-04-12 18:43:05 -0400

    changed seed data to spanish

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Spanish', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'cerveza', 'beer', 2),
+  (2, 1, 'mañana', 'tomorrow', 3),
+  (3, 1, 'gracias', 'thanks', 4),
+  (4, 1, 'necesito', 'I need', 5),
+  (5, 1, 'bien', 'good', 6),
+  (6, 1, 'mal', 'bad', 7),
+  (7, 1, 'grande', 'big', 8),
+  (8, 1, 'gato', 'cat', 9),
+  (9, 1, 'pequeño', 'small', 10),
+  (10, 1, 'perro', 'dog', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
--- test/setup.js
+++ test/setup.js
@@ -6,7 +6,7 @@ process.env.JWT_EXPIRY = '3m'
 require('dotenv').config()
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder-mifflin@localhost/spaced-repetition-test"
+  || "postgresql://dunder_mifflin@localhost/spaced-repetition-test"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
