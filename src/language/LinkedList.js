5bfcd86ec9d113d064033c0a49b473fdca150693
Matthew Wagaman <averaqedev@gmail.com>
2020-04-16 09:22:10 -0700

    rewrite post guess

--- src/language/LinkedList.js
+++ src/language/LinkedList.js
@@ -6,12 +6,10 @@ class _Node {
 }
 
 class LinkedList {
-  constructor(user_id, id, total_score = 0) {
-    this.user_id = user_id;
-    this.id = id;
-    this.total_score = total_score;
+  constructor() {
     this.head = null;
   }
+
   insertFirst(item) {
     this.head = new _Node(item, this.head);
   }
@@ -19,7 +17,8 @@ class LinkedList {
   insertLast(item) {
     if (this.head === null) {
       this.insertFirst(item);
-    } else {
+    }
+    else {
       let tempNode = this.head;
       while (tempNode.next !== null) {
         tempNode = tempNode.next;
@@ -28,48 +27,98 @@ class LinkedList {
     }
   }
 
-  relocateHead(memoryValue) {
-    let tempNode = this.head.next;
-    let beforeNode = this._findNthElement(memoryValue);
-    this.head.next = beforeNode.next;
-    beforeNode.next = this.head;
-    this.head = tempNode;
-    return [beforeNode, beforeNode.next];
-  }
-
-  _findNthElement(position) {
-    let node = this.head;
-    for (let i = 0; i < position; i++) {
-      if(!node.next) {
-        return node;
+  find(item) { 
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item 
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+           and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
       }
-      node = node.next;
+      else {
+        // Otherwise, keep looking 
+        currNode = currNode.next;
+      }
     }
-    return node;
+    // Found it
+    return currNode;
   }
 
-  forEach(cb) {
-    let node = this.head;
-    const arr = [];
-    while (node) {
-      arr.push(cb(node));
-      node = node.next;
+  remove(item){ 
+    // If the list is empty
+    if (!this.head) {
+      return null;
     }
-    return arr;
-  }
-
-  size(list) {
-    let nodeCounter = 1;
-    if (list.head === null) {
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
       return;
     }
-    let currentNode = list.head;
-    while (currentNode.next !== null) {
-      currentNode = currentNode.next;
-      nodeCounter++;
+    // Start at the head
+    let currNode = this.head;
+    // Keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      // Save the previous node 
+      previousNode = currNode;
+      currNode = currNode.next;
     }
-    return nodeCounter;
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
   }
+
+  insertBefore(newItem, oldItem) {
+    // If the list is empty
+    if (!oldItem) {
+      return this.insertFirst(newItem);
+    }
+    let tempNode = this.head;
+    let oldNode = this.find(oldItem);
+    let newNode = new _Node(newItem, oldNode);
+    while(tempNode.next !== null){
+      if(tempNode.next.value === oldNode.value) {
+        break;
+      }
+      tempNode = tempNode.next;
+    }
+    tempNode.next = newNode;
+  }
+
+  insertAfter(newItem, oldItem) {
+    let oldNode = this.find(oldItem);
+    let newNode = new _Node(newItem, oldNode.next);
+    oldNode.next = newNode;
+  }
+
+  insertAt(newItem, pos) {
+    let tempNode = this.head;
+    for(let i = 1; i<pos; i++){
+      if(!tempNode){
+        return 'Postion out of bounds';
+      }
+      tempNode = tempNode.next;
+    }
+    this.insertBefore(newItem, tempNode.value);
+  }
+
+  print(){
+    let temp = this.head;
+    while(temp.next != null){
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
 }
 
 module.exports = LinkedList;
--- src/language/language-router.js
+++ src/language/language-router.js
@@ -19,7 +19,7 @@ languageRouter.use(requireAuth).use(async (req, res, next) => {
 
     if (!language)
       return res.status(404).json({
-        error: "You don't have any languages",
+        error: 'You don\'t have any languages',
       });
 
     req.language = language;
@@ -47,7 +47,6 @@ languageRouter.get('/', async (req, res, next) => {
 });
 
 languageRouter.get('/head', async (req, res, next) => {
-  console.log(req.language);
   LanguageService.getLanguageHead(req.app.get('db'), req.language.head)
     .then((nextHead) => {
       const {
@@ -72,49 +71,84 @@ languageRouter.post('/guess', bodyParser, async (req, res, next) => {
 
   if (!guess) {
     return res.status(400).json({
-      error: "Missing 'guess' in request body",
+      error: 'Missing \'guess\' in request body',
     });
   }
 
   try {
-    const words = await LanguageService.getLanguageWords(
+    let list = new LinkedList();
+    let word = await LanguageService.populate(
       req.app.get('db'),
-      req.language.id
+      req.language.id, 
+      req.language.head
     );
+    while(word.rows[0].next != null) {
+      list.insertLast(word.rows[0].original);
+      word = await LanguageService.populate(
+        req.app.get('db'), 
+        req.language.id, 
+        word.rows[0].next
+      );
+    }
+    list.insertLast(word.rows[0].original);
+    list.insertLast(word.rows[0].insertLast);
 
-    let SLL = await LanguageService.createLinkedList(req.language, words);
+    let answer = await LanguageService.getTrans(req.app.get('db'), req.language.id, list.head.value);
+    answer = answer.rows[0].translation.trim();
+    answer = answer.toLowerCase();
 
-    const answer = SLL.head.value.translation.toLowerCase();
+    const values = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value);
+    let { memory_value, correct_count, incorrect_count, total_score } = values.rows[0];
     let isCorrect;
-    if (guess.toLowerCase() === answer) {
+
+    if(guess === answer){
       isCorrect = true;
-      SLL.head.value.memory_value *= 2;
-      SLL.head.value.correct_count++;
-      SLL.total_score++;
-    } else {
+      memory_value *= 2;
+      correct_count += 1;
+      total_score += 1;
+    }
+    else {
       isCorrect = false;
-      SLL.head.value.memory_value = 1;
-      SLL.head.value.incorrect_count++;
+      memory_value = 1;
+      incorrect_count += 1;
     }
 
-    const relocateWords = await SLL.relocateHead(SLL.head.value.memory_value);
+    //save the new values to the word in the database
+    const newValues = {
+      memory_value, 
+      correct_count, 
+      incorrect_count, 
+      total_score
+    };
+    LanguageService.setValues(req.app.get('db'), req.language.id, list.head.value, newValues);
 
-    await LanguageService.updateHead(
-      req.app.get('db'),
-      req.language.id,
-      SLL.head.id
-    );
-    await LanguageService.updateTotalScore(req.app.get('db'), SLL);
-    await LanguageService.updateWords(req.app.get('db'), relocateWords);
+    //remove the current word and place it however many steps back
+    let temp = list.head.value;
+    list.remove(list.head.value);
+    list.insertAt(temp, memory_value+1);
 
-    return res.status(200).json({
-      nextWord: SLL.head.value.original,
-      wordCorrectCount: SLL.head.value.correct_count,
-      wordIncorrectCount: SLL.head.value.incorrect_count,
-      totalScore: SLL.total_score,
+    //get next word and its values for correct and incorrect
+    const nextValues = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value);
+    correct_count = nextValues.rows[0].correct_count;
+    incorrect_count = nextValues.rows[0].incorrect_count;
+
+    //save the order of the linked list to the database
+    const headID = await LanguageService.getWordID(req.app.get('db'), req.language.id, list.head.value);
+    LanguageService.setHead(req.app.get('db'), req.language.id, headID.rows[0].id);
+    const current_ID = await LanguageService.getWordID(req.app.get('db'), req.language.id, temp);
+    const next_next = await LanguageService.getNextID(req.app.get('db'), req.language.id, current_ID.rows[0].id+memory_value);
+    LanguageService.savePlacement(req.app.get('db'), current_ID.rows[0].id, current_ID.rows[0].id+memory_value, next_next.rows[0].next);
+
+    //send back the required fields
+    const output = {
+      nextWord: list.head.value,
+      wordCorrectCount: correct_count,
+      wordIncorrectCount: incorrect_count,
+      totalScore: total_score,
       answer,
       isCorrect,
-    });
+    };
+    res.json(output);
   } catch (error) {
     next(error);
   }
